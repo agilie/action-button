fb522e9b09a2ac39d4c658a5f9ae5753a7c7400d
alex_klymenko <klymenko.sasha21@gmail.com>
2018-12-26 18:08:00 +0200

    update package dist folder / event handlers

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-/*Settings:
-	2. items gap(distance from action-button)
-Errors:
-	- if items more then 3
-	- if items gap less than (action-button radius + items radius + 20)
-*/
+module.exports =  ActionBtn;
 function ActionBtn(elemsContainer, options = {}) {
 	this.getOpenStatus = function() {
 		return isOpen;
@@ -34,11 +29,11 @@ function ActionBtn(elemsContainer, options = {}) {
 	var timer1;
 	var timer2;
 	var defaultOptions = {
-		actionBtnSize: 100,
+		actionBtnSize: 80,
 		actionBtnColor: 'rgb(249, 180, 120)',
-		itemsSize: 60,
+		itemsSize: 45,
 		itemsColors: ['rgb(117, 174, 253)', 'rgb(247, 113, 109)', 'rgb(251, 213, 112)'],
-		itemsGap: 100,
+		itemsGap: 20,
 		itemsOpenType: 'radial',
 		itemsOpenDirection: 'top-left'
 	}
@@ -48,6 +43,7 @@ function ActionBtn(elemsContainer, options = {}) {
 		btn: createActionBtn(settings.actionBtnSize, settings.actionBtnColor),
 		items: Array.prototype.slice.call(elemsContainer.querySelectorAll('*'))
 	}
+	var wrapper = createWrapper();
 	var items = elemsContainer.children;
 	var containerBackground = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="0" height="0"><defs><filter id="goo"><feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" /><feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" /><feBlend in="SourceGraphic" in2="goo" /></filter></defs></svg>';
 	var actionBtnOpened = new CustomEvent("ActionBtnOpened", {
@@ -86,11 +82,17 @@ function ActionBtn(elemsContainer, options = {}) {
 		clearTimeout(timer2);
 	}
 	function init() {
+		checkItemsNumber(elems.items);
 		configurateItems();
 		elems.container.insertBefore(elems.btn, elems.container.firstChild);
 		elems.container.insertAdjacentHTML('afterend', containerBackground);
 		elems.container.classList.add('addBtn__wrap');
-		elems.container.classList.add(setOpenDirection(settings.itemsOpenDirection));
+
+		var parentElem = elems.container.parentNode;
+		parentElem.appendChild(wrapper);
+		wrapper.appendChild(elems.container);
+
+		// elems.container.classList.add(setOpenDirection(settings.itemsOpenDirection));
 	}
 	init();
 	function setOpenType(type = 'radial') {
@@ -121,29 +123,30 @@ function ActionBtn(elemsContainer, options = {}) {
 		isOpen ? closeItems() : openItems();
 	}
 	function openItems() {
-		var toggleClass = setOpenType(settings.itemsOpenType),
-				btnElem = elems.btn;
+		var elemsTranforms = calcRadialTransform();
+		var btnElem = elems.btn;
 
+		setWrapSize();
 		btnElem.classList.remove('addBtn--close');
 		btnElem.classList.add('addBtn--open');
 		btnElem.style.animationIterationCount = items.length - 1;
-		for(var i = 1; i < items.length; i++) {
-			items[i].classList.add(toggleClass);
-		}
+		elems.items.forEach(function(item, index) {
+			item.style.transform = `translate(${elemsTranforms[index].x}, ${elemsTranforms[index].y})`;
+		})
 		isOpen = true;
 		stopTimer2();
 		startTimer1();
 	}
 	function closeItems() {
-		var toggleClass = setOpenType(settings.itemsOpenType),
-				btnElem = elems.btn;
+		var btnElem = elems.btn;
 
+		removeWrapSize();
 		btnElem.classList.remove('addBtn--open');
 		btnElem.classList.add('addBtn--close');
 		btnElem.style.animationIterationCount = items.length - 1;
-		for(var i = 1; i < items.length; i++) {
-			items[i].classList.remove(toggleClass);
-		}
+		elems.items.forEach(function(item, index) {
+			item.style.transform = 'translate(0)';
+		})
 		isOpen = false;
 		stopTimer1();
 		startTimer2();
@@ -170,6 +173,70 @@ function ActionBtn(elemsContainer, options = {}) {
 		})
 		animDuration = parseFloat(items[items.length - 1].style.transitionDelay) + parseFloat(items[items.length - 1].style.transitionDuration);
 	}
+	function calcRadialTransform(distance = settings.itemsGap) {
+		if (checkGap(distance)) {
+			var arrOfTransforms = [];
+			var realGap = distance + (settings.actionBtnSize / 2) + (settings.itemsSize / 2);
+			var oxDistance = realGap * Math.cos(45 * Math.PI / 180);
+			var oyDistance = realGap * Math.sin(45 * Math.PI / 180);
+			var firstElem, secondElem, thirdElem, fourElem, fiveElem;
+			var firstElem = {
+				x: '0px',
+				y: `-${realGap}px`
+			};
+			var secondElem = {
+				x: `-${oxDistance}px`,
+				y: `-${oyDistance}px`
+			};
+			var thirdElem = {
+				x: `-${realGap}px`,
+				y: '0px'
+			};
+			var fourElem = {
+				x: `-${oxDistance}px`,
+				y: `${oyDistance}px`
+			};
+			var fiveElem = {
+				x: '0px',
+				y: `${realGap}px`
+			};
+			arrOfTransforms.push(firstElem, secondElem, thirdElem, fourElem, fiveElem);
+		} else {
+			throw new Error('Please set more gap between Action-button and items.');
+		}
+
+		return arrOfTransforms;
+	}
+	function checkGap(gap) {
+		return gap > 0 ? true : false;
+	}
+	function checkItemsNumber(items) {
+		if (items.length > 5) {
+			throw new Error('Max items count is 5!!!');
+		}
+	}
+	function calcWrapSize() {
+		var size = ((settings.actionBtnSize / 2) + settings.itemsGap + settings.itemsSize + 20) * 2;
+
+		return size;
+	}
+	function setWrapSize() {
+		elemsContainer.classList.add('addBtn__wrap--active');
+		elemsContainer.style.width = `${calcWrapSize()}px`;
+		elemsContainer.style.height = `${calcWrapSize()}px`;
+	}
+	function removeWrapSize() {
+		elemsContainer.classList.remove('addBtn__wrap--active');
+		elemsContainer.style.width = 'auto';
+		elemsContainer.style.height = 'auto';
+	}
+	function createWrapper() {
+		var wrap = document.createElement('div');
+		
+		wrap.classList.add('addBtn__wrapper');
+		wrap.style.width = `${settings.actionBtnSize}px`;
+		wrap.style.height = `${settings.actionBtnSize}px`;
+
+		return wrap;
+	}
 }
-
-
